97ddb533d8ba72522eb5e2c8d2df97d2869a1a90
Daxow <voxizofficiel@gmail.com>
2025-12-01 08:38:20 +0100

    add templates

--- web/handler.go
+++ web/handler.go
@@ -1,29 +1,43 @@
 package web
 
 import (
-	"log"
-	"net/http"
-	"power4/game"
+    "html/template"
+    "log"
+    "net/http"
+
+    "power4/game"
 )
 
 type Server struct {
-	Game *game.Game
+    Game *game.Game
+    Tmpl *template.Template
 }
 
 func NewServer() *Server {
-	g := game.NewGame()
-	return &Server{
-		Game: &g,
-	}
+    g := game.NewGame()
+    t, err := template.ParseFiles("templates/index.html")
+    if err != nil {
+        log.Fatal("Error parsing templates:", err)
+    }
+    return &Server{
+        Game: &g,
+        Tmpl: t,
+    }
 }
 
 func (s *Server) RegisterRoutes() {
-	http.HandleFunc("/", s.handleIndex)
+    http.HandleFunc("/", s.handleIndex)
 }
 
 func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Puissance 4"))
-	if err != nil {
-		log.Println("write error:", err)
-	}
+    if r.Method != http.MethodGet {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
+    err := s.Tmpl.Execute(w, s.Game)
+    if err != nil {
+        http.Error(w, "Error rendering template", http.StatusInternalServerError)
+        return
+    }
 }
